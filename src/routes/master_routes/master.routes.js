c0519752d29a4a75ea2d8b55378ac9952005c210
romirahmann <romirahman03romi@gmail.com>
2025-01-11 10:29:25 +0700

    NEW: Validasi Routes

--- src/routes/master_routes/master.routes.js
+++ src/routes/master_routes/master.routes.js
@@ -1,21 +1,42 @@
 const express = require("express");
 const router = express.Router();
+const Joi = require("joi");
+
 const routesData = require("../../database/routes.data");
 const TestController = require("../../controllers/master_controller/testController");
 
-// MAPING CONTROLLER
 const controllers = {
   TestController,
 };
 
+// Definisikan skema validasi untuk setiap rute
+const routeSchema = Joi.object({
+  method: Joi.string().valid("get", "post", "put", "delete").required(),
+  path: Joi.string().required(),
+  controller: Joi.string().required(),
+});
+
+// Validasi setiap routeData dengan skema yang telah ditentukan
 routesData.forEach((route) => {
+  const { error } = routeSchema.validate(route);
+  if (error) {
+    console.error(
+      `Invalid route configuration for path '${route.path}': ${error.details[0].message}`
+    );
+    return; // Mengabaikan rute yang invalid dan melanjutkan ke rute berikutnya
+  }
+
+  // Mendaftarkan route jika valid
   const [controllerName, methodName] = route.controller.split(".");
   const controller = controllers[controllerName];
 
-  if (controller && typeof controller[methodName] == "function") {
+  if (controller && typeof controller[methodName] === "function") {
+    // Menambahkan route ke router jika controller dan method ditemukan
     router[route.method](route.path, controller[methodName]);
   } else {
-    console.error(`Controller or method not found: ${route.controller}`);
+    console.error(
+      `Controller or method not found for route '${route.path}': ${route.controller}`
+    );
   }
 });
 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,19 +1,17 @@
 var express = require("express");
 var router = express.Router();
 const path = require("path");
-
 const masterRoutes = require("./master_routes/master.routes");
 const auth_routes = require("./utility_routes/auth.routes");
 
 const { accessControl, verifyToken } = require("../services/auth.service");
 
-// Route untuk halaman error
 router.get("/error", function (req, res) {
   res.status(404).sendFile(path.join(__dirname, "../views/errorPage.html"));
 });
 
 router.use("/auth/", auth_routes);
-// router.use("/master/", accessControl, masterRoutes);
+
 router.use("/master/", masterRoutes);
 
 module.exports = router;
